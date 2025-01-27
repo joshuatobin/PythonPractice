931e10e513470b087357373841c9b3287ba4bd6b
Joshua Tobin <joshuatobin@gmail.com>
2025-01-27 09:08:17 -0500

    Adding zoo OOP

--- ZooManagementOOP/main.py
+++ ZooManagementOOP/main.py
@@ -15,21 +15,38 @@ class Zoo():
         """
         The add() method here would add an animal to the zoo’s internal list.
         """
+        
+        # NOTE: This works because Python is dynamically typed and does not enforce a specific type for the parameter species in your add() method
+        # Python doesn’t care that the parameter is named species; it just treats species as whatever you pass in, whether that’s a Mammal, Animal, or any other object.
         self.animals.append(species)
 
-    def remove(self, species):
+    def remove(self, name):
         """
         The remove() method would remove an animal by some unique identifier (like name or species).
         """
 
+        print(f'Removing the animal called: {name}')
+
         # Remember this overrides the list so there no need to call remove from the list
-        self.animals = [animal for animal in self.animals if animal.species != species ]
+        self.animals = [animal for animal in self.animals if animal.name != name ]
+
+        # Note: A for loop or list comprehension in Python doesn’t raise an exception if an element isn’t found. It just returns a new list so there is no need 
+        # to wrap in try/except logic
 
     def all(self):
         """
         List all animals, grouped by category (Mammals, Birds, Reptiles).
         """
-        print(self.animals)
+        for animal in self.animals:
+            if isinstance(animal, Mammal):
+                print(f"Mammals: {animal}")
+            elif isinstance(animal, Bird):
+                print(f"Birds: {animal}")
+            elif isinstance(animal, Reptile):
+                print(f"Reptile: {animal}")
+            else:
+                print("ERROR not found")
+            
 
 class Animal():
     """
@@ -84,7 +101,7 @@ def main():
     zoo.add(lion)
     
     # Remove the lion but by using the species itself, not the instance class. 
-    zoo.remove(lion.species)
+    zoo.remove(lion.name)
 
     lion = Mammal(name="lion", species="lion", age=5)
     bird = Bird(name="bird", species="bird", age=6)
@@ -96,6 +113,8 @@ def main():
     zoo.add(gator)
 
     zoo.all()
+    
+    zoo.remove('foo')
     
 if __name__ == "__main__":
     main()
